6f6c793eda0b4c6b6dadb9ac779ad1947d0a7729
冯凯超 <kaichao.feng@w-group.com.cn>
2022-08-19 11:39:35 +0800

    fix: 修复ts报错

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,6 @@
 import Request from "@/utils/request";
 export const Login = (data:(Service.LoginReq)) => {
-    return Request<Service.LoginRes>({
+    return Request({
         method: "post",
         url: "/login",
         data
