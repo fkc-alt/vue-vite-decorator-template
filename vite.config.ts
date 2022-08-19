0552eac1c73f3cc07c6935d885ed44b68533ef93
冯凯超 <kaichao.feng@w-group.com.cn>
2022-08-19 15:10:26 +0800

    chore: 修改构建工具

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig, loadEnv } from 'vite';
 import { resolve } from 'path';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx' 
+import Vue from '@vitejs/plugin-vue';
+import VueJsx from '@vitejs/plugin-vue-jsx' 
+import { terser } from "rollup-plugin-terser";
+import Resolve from "@rollup/plugin-node-resolve"
 /**
   * 在setup语法糖中，解决无法自定义组件的 name 属性
   * 使用方法  defineOptions({ name: 'my-component' })
@@ -9,19 +11,33 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import DefineOptions from 'unplugin-vue-define-options/vite';
 // 解决控制台warning
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+// 开启build打包压缩
+import ViteCompression from 'vite-plugin-compression';
+
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return defineConfig({
     plugins: [
-      vue(), 
-      vueJsx(),
+      Vue(), 
+      VueJsx(),
       DefineOptions(),
+      Resolve(),
+      terser(),
+      VueI18nPlugin(),
       VueI18nPlugin({
         /* options */
         // locale messages resource pre-compile option
         include: resolve(__dirname, './src/locales/**'),
       }),
+      ViteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: 'gzip',
+        ext: '.gz',
+      }),
+      
     ],
     resolve: {
       alias: {
@@ -50,6 +66,30 @@ export default ({ mode }) => {
           additionalData: `@import "@/styles/mixin.scss";`
         }
       }
+    },
+    build: {
+      sourcemap: mode === "dev",
+      // 大文件报警阈值设置,不建议使用
+      // chunkSizeWarningLimit: 1500,
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        }
+      },
+      rollupOptions: {
+        output: {
+          manualChunks(id){
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+        },
+      },
     }
   })  
 } 
