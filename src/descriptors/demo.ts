f10938e7af2a569af5925b8969c0546c4e3c2f1f
冯凯超 <v_kaicfeng@tencent.com>
2022-10-30 21:33:12 +0800

    eslint --fix

--- src/descriptors/demo.ts
+++ src/descriptors/demo.ts
@@ -1,43 +1,11 @@
-const params = { name: '冯凯超', age: 22 }
-
-type Params = typeof params
-
-function descriptors<T extends object, U> (params: T): Function {
-  return function (target: any, name: string, descriptor: any) {
-    const oldDescriptor: Function = descriptor.value
-    descriptor.value = function (args: U): void {
-      Object.keys(params).forEach(key => target[key] = params[key as keyof T])
-      target.NumList = args
-      oldDescriptor.call(this, ...arguments)
-    }
-  }
-}
-
-class Demo {
-  private readonly num = 20
-  @descriptors<Params, number[]>(params)
-  public Log<T> (args: T): void {
-    console.log('Log', this, arguments)
-  }
-}
-
-function demo () {
+function demo (): Record<'name', string> {
   return {
     name: '1'
   }
 }
 
-type GetReturnData<T extends Function> = T extends (...args: any) => infer U ? U : never
-type arr<T> = T extends Array<infer U> ? U : never
-type ar = arr<string[]>
-type tuple<T> = T extends [...args: infer U] ? U : never
-type pup = tuple<[number, string]>
+type GetReturnData<T extends () => unknown> = T extends (...args: unknown[]) => infer U ? U : never
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type d = GetReturnData<typeof demo>
 
-type laststr<T extends string> = T extends `${infer U}${infer _R}` ? U : never
-type Pro<T> = T extends Promise<infer U> ? U : never
-
-type Promi = Pro<Promise<GetReturnData<typeof demo>>>
-type str = laststr<'123'>
-
-export default new Demo()
+export default demo
