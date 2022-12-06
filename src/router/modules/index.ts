82315a78c24cb410e90c84c04e37f4990ad724f1
冯凯超 <kaichao.feng@w-group.com.cn>
2022-12-06 14:56:25 +0800

    feat: 简化代码

--- src/router/modules/index.ts
+++ src/router/modules/index.ts
@@ -3,9 +3,6 @@
  */
 const modulesFiles: Common.Glob = import.meta.glob('/src/router/modules/**/*.ts', { import: 'default', eager: true })
 
-const modules = Object.values(modulesFiles).reduce((prev, next) => {
-  prev = [...prev, ...next]
-  return prev
-}).sort((a, b) => (a.sort != null) && (b.sort != null) ? a.sort - b.sort : -1)
+const modules = Object.values(modulesFiles).reduce((prev, next) => [...prev, ...next]).sort((a, b) => (a.sort != null) && (b.sort != null) ? a.sort - b.sort : -1)
 
 export default modules
