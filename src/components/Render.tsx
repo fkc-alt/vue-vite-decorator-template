9e48e8f568f79eedd4fe819ab02ebf2636e9867d
冯凯超 <kaichao.feng@w-group.com.cn>
2022-12-16 16:37:41 +0800

    feat: update Render

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -1,7 +1,5 @@
-import Table from './Table'
-
 const Render = (props: unknown): JSX.Element => {
   // 注意点：在tsx之中 不会自动读写 X.value
-  return <Table data={[]} headers={[]} />
+  return <div {...(props) as Record<string, string>}>TSX Component</div>
 }
 export default Render
