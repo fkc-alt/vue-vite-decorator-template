18bad87429dd48fdc946b67ea3da046b909cffc5
冯凯超 <kaichao.feng@w-group.com.cn>
2022-12-30 13:43:30 +0800

    feat: 优化代码结构

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,33 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ElTable, ElTableColumn } from 'element-plus'
-
-const handlerMethods = (methods: ElTableCustom.TableEvents): ElTableCustom.TableEvents => {
-  const mapMehods = {
-    onSelect: methods.onSelect,
-    'onExpand-change': methods.onExpandChange ?? methods['onExpand-change'],
-    'onCurrent-change': methods.onCurrentChange ?? methods['onCurrent-change'],
-    'onSelect-all': methods.onSelectAll ?? methods['onSelect-all'],
-    'onSelection-change': methods.onSelectionChange ?? methods['onSelection-change'],
-    'onCell-mouse-enter': methods.onCellMouseEnter ?? methods['onCell-mouse-enter'],
-    'onCell-mouse-leave': methods.onCellMouseLeave ?? methods['onCell-mouse-leave'],
-    'onCell-contextmenu': methods.onCellContextmenu ?? methods['onCell-contextmenu'],
-    'onCell-click': methods.onCellClick ?? methods['onCell-click'],
-    'onCell-dblclick': methods.onCellDblclick ?? methods['onCell-dblclick'],
-    'onRow-click': methods.onRowClick ?? methods['onRow-click'],
-    'onRow-contextmenu': methods.onRowContextmenu ?? methods['onRow-contextmenu'],
-    'onRow-dblclick': methods.onRowDblclick ?? methods['onRow-dblclick'],
-    'onHeader-click': methods.onHeaderClick ?? methods['onHeader-click'],
-    'onHeader-contextmenu': methods.onHeaderContextmenu ?? methods['onHeader-contextmenu'],
-    'onSort-change': methods.onSortChange ?? methods['onSort-change'],
-    'onFilter-change': methods.onFilterChange ?? methods['onFilter-change'],
-    'onHeader-dragend': methods.onHeaderDragend ?? methods['onHeader-dragend']
-  }
-  return (Object.keys(mapMehods) as unknown as Array<keyof(typeof mapMehods)>).filter(key => mapMehods[key]).reduce((prev, key) => ({ ...prev, [key]: mapMehods[key] }), {})
-}
+import { handlerEvents } from './utils'
 
 export default function (props: ElTableCustom.TableProps<any>): JSX.Element {
   const { headers, ...attributes } = props
-  Object.assign(attributes, handlerMethods(attributes))
+  Object.assign(attributes, handlerEvents(attributes))
   return (
     <ElTable { ...attributes }>
       {headers.map(attributes => {
@@ -38,7 +15,10 @@ export default function (props: ElTableCustom.TableProps<any>): JSX.Element {
               column,
               index: $index,
               cellValue: attributes
-            }) ?? attributes?.formatter?.(row, column, row[attributes?.prop as string], $index) ?? row[attributes?.prop as string]
+            }) ?? attributes?.formatter?.(row, column, row[attributes?.prop as string], $index) ?? row[attributes?.prop as string],
+            [attributes.prop as string]: (row: ElTableCustom.Cell) => {
+              return <slot v-slots={attributes.prop}></slot>
+            }
           }}
         </ElTableColumn>
       })}
---
+++ src/components/Table/utils.ts
@@ -0,0 +1,23 @@
+export const handlerEvents = (methods: ElTableCustom.TableEvents): ElTableCustom.TableEvents => {
+  const mapEvents = {
+    onSelect: methods.onSelect,
+    'onExpand-change': methods.onExpandChange ?? methods['onExpand-change'],
+    'onCurrent-change': methods.onCurrentChange ?? methods['onCurrent-change'],
+    'onSelect-all': methods.onSelectAll ?? methods['onSelect-all'],
+    'onSelection-change': methods.onSelectionChange ?? methods['onSelection-change'],
+    'onCell-mouse-enter': methods.onCellMouseEnter ?? methods['onCell-mouse-enter'],
+    'onCell-mouse-leave': methods.onCellMouseLeave ?? methods['onCell-mouse-leave'],
+    'onCell-contextmenu': methods.onCellContextmenu ?? methods['onCell-contextmenu'],
+    'onCell-click': methods.onCellClick ?? methods['onCell-click'],
+    'onCell-dblclick': methods.onCellDblclick ?? methods['onCell-dblclick'],
+    'onRow-click': methods.onRowClick ?? methods['onRow-click'],
+    'onRow-contextmenu': methods.onRowContextmenu ?? methods['onRow-contextmenu'],
+    'onRow-dblclick': methods.onRowDblclick ?? methods['onRow-dblclick'],
+    'onHeader-click': methods.onHeaderClick ?? methods['onHeader-click'],
+    'onHeader-contextmenu': methods.onHeaderContextmenu ?? methods['onHeader-contextmenu'],
+    'onSort-change': methods.onSortChange ?? methods['onSort-change'],
+    'onFilter-change': methods.onFilterChange ?? methods['onFilter-change'],
+    'onHeader-dragend': methods.onHeaderDragend ?? methods['onHeader-dragend']
+  }
+  return (Object.keys(mapEvents) as unknown as Array<keyof(typeof mapEvents)>).filter(key => mapEvents[key]).reduce((prev, key) => ({ ...prev, [key]: mapEvents[key] }), {})
+}
