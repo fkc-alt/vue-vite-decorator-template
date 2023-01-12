1397159b052196026ede6a437a80dd6f14ca2267
冯凯超 <kaichao.feng@w-group.com.cn>
2023-01-12 16:57:31 +0800

    feat: @eslintrc add Typescript rules

--- src/router/modules/index.ts
+++ src/router/modules/index.ts
@@ -4,8 +4,7 @@
 const modulesFiles: Common.Glob = import.meta.glob('/src/router/modules/**/*.ts', { import: 'default', eager: true })
 
 const routesSort = (routes: Array<import('vue-router').RouteRecordRaw>): void => {
-  routes.sort((a, b) => (a.sort != null) && (b.sort != null) ? a.sort - b.sort : -1)
-  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  routes.sort((a, b) => a.sort && b.sort ? a.sort - b.sort : -1)
   routes.forEach(route => !!route.children?.length && routesSort(route.children))
 }
 
