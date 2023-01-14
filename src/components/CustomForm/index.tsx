2e43994ade0fa63c4e7c7a5a752d2e8a7f2b6b12
Jack <fengkaichao149@163.com>
2023-01-14 13:57:50 +0800

    style: @component 优化传参方式

--- src/components/CustomForm/index.tsx
+++ src/components/CustomForm/index.tsx
@@ -9,13 +9,20 @@ import { randomKey } from '@/utils'
  * @description ElForm二次封装
  */
 export default defineComponent({
+  /**
+   * @description 该参数并不在ElForm的props中定义，如果使用attrs的话,会绑定到根元素上.此处定义props是为了防止此原因
+   */
+  props: {
+    formItems: Array<CustomerProps.CustomForm.Component>
+  },
   setup (props, { attrs, emit, expose, slots }) {
-    const { formItems, ...attributes } = attrs as unknown as CustomerProps.CustomForm.CustomFormProps
+    const { formItems } = toRefs(props)
+    const { ...attributes } = attrs as unknown as CustomerProps.CustomForm.CustomFormProps
     const formRef = ref<FormInstance>()
     expose({ formRef })
     return () => (
       <ElForm {...attributes} ref={formRef} key={randomKey()}>
-        {formItems.map((attribute, index) => {
+        {formItems.value?.map((attribute, index) => {
           const { component, option, slots, events, ...props } = attribute
           return <ElFormItem {...props}>
             {{
