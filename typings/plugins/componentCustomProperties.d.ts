6a69a2b45997a8f6ebe8a718c1d84b702b2e021f
kaichao.feng <fengkaichao149@163.com>
2023-01-17 16:28:49 +0800

    feat: typings

--- typings/plugins/componentCustomProperties.d.ts
+++ typings/plugins/componentCustomProperties.d.ts
@@ -2,7 +2,6 @@ export {}
 
 declare module 'vue' {
   interface ComponentCustomProperties {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     Test: string
     NotImagesNot: string
     NotImagesNotCloud: string
