7259cee8969fd4a9ee278557211eb79ab8a46871
Jack <fengkaichao149@163.com>
2023-02-25 22:46:48 +0800

    feat: upload

--- src/service/providers/upload.service.ts
+++ src/service/providers/upload.service.ts
@@ -7,11 +7,11 @@ export default class UploadService {
   constructor (private readonly requestService: RequestService) { }
 
   public async uploadFile<T extends AxiosRequestConfig<Services.Common.UplaodReq>, U = Services.Common.UplaodRes>(configure: T): ServerRes<U> {
-    const { data: { file, ...params } = { }, ...reqJson } = configure
+    const { data: { file, ...params } = { }, ...requestConfig } = configure
     const fileLoder = new FormData()
     fileLoder.append('file', <Blob>file?.raw)
     return await this.requestService.request<Services.Common.UplaodFileReq, U>({
-      ...reqJson,
+      ...requestConfig,
       data: { file: <FormDataEntryValue>fileLoder.get('file'), ...(params || {}) },
       headers: { 'Content-Type': 'multipart/form-data' }
     })
@@ -21,16 +21,18 @@ export default class UploadService {
     type UploadFile = Services.Common.UplaodReq extends { file: infer U } ? U : never
     return await new Promise((resolve, reject) => {
       try {
-        const { data: { file, ...params } = {}, ...reqJson } = configure
+        const { data: { file, ...params } = {}, ...requestConfig } = configure
         const render = new FileReader()
         render.onload = (e: ProgressEvent<FileReader>) => {
           const base64 = (<string>e.target?.result)?.split(',').pop() ?? ''
           const ext = `.${<string>((<UploadFile>file).name.split('.').pop())}`
-          resolve(this.requestService.request<Services.Common.UplaodBase64Req, U>({ ...reqJson, data: { base64, ext, ...(params || {}) } }))
+          resolve(this.requestService.request<Services.Common.UplaodBase64Req, U>({ ...requestConfig, data: { base64, ext, ...(params || {}) } }))
         }
         render.readAsDataURL(<Blob>(<UploadFile>file).raw)
       } catch (error) {
         reject(error)
+      } finally {
+        console.log('upload success')
       }
     })
   }
--- src/store/user.ts
+++ src/store/user.ts
@@ -18,17 +18,13 @@ export const useUserStore = defineStore('user', {
     },
     generateRoutes (router: Router) {
       const cloneRouter: Router = JSON.parse(JSON.stringify(router))
-      const routes = cloneRouter.options.routes.filter(
-        (v) => v.path !== '/login' && !v.hidden
-      )
+      const routes = cloneRouter.options.routes.filter((v) => v.path !== '/login' && !v.hidden)
       this.routes = this.handleTreeRoutes(this.handleMapRoutes(routes))
       this.routesSort(this.routes)
     },
     handleTreeRoutes (routes: RouteRecordRaw[]): RouteRecordRaw[] {
       return routes.filter((v) => {
-        if (v.meta?.roles?.length) {
-          return v.meta.roles.some((o) => this.roleIdList?.includes(o))
-        }
+        if (v.meta?.roles?.length) return v.meta.roles.some((o) => this.roleIdList?.includes(o))
         if (v?.children?.length) {
           v.children = this.handleTreeRoutes(v.children)
           return !!v.children?.length && !v.children.every((o) => o.hidden)
@@ -39,12 +35,8 @@ export const useUserStore = defineStore('user', {
     },
     handleMapRoutes (routes: RouteRecordRaw[]): RouteRecordRaw[] {
       return routes.map((v) => {
-        if (v?.children?.length === 1 && v?.meta?.alwaysShow) {
-          v = v.children[0]
-        }
-        if (v?.children && v?.children.length > 1) {
-          v.children = this.handleMapRoutes(v.children)
-        }
+        if (v?.children?.length === 1 && v?.meta?.alwaysShow) v = v.children[0]
+        if (v?.children && v?.children.length > 1) v.children = this.handleMapRoutes(v.children)
         return v
       })
     },
