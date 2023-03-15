48a53eda6fe8333bf6c6dac029e3fa5a37e5c347
Jack <fengkaichao149@163.com>
2023-03-15 23:17:33 +0800

    feat: apply

--- src/main.ts
+++ src/main.ts
@@ -26,4 +26,5 @@ const bootstrap = (): void => {
   }).catch(console.error)
 }
 console.log(application, 'test')
+application.demoService.Log({ id: '', price: 121233 }, { name: '123' })
 bootstrap()
