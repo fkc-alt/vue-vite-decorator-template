06d113a9f5329f20586da1403e4e7ee8be634be0
Jack <fengkaichao149@163.com>
2023-04-03 23:02:23 +0800

    feat: description

--- src/service/modules/article/article.service.ts
+++ src/service/modules/article/article.service.ts
@@ -17,11 +17,11 @@ class CustomValidationPipe implements Core.PipeTransform {
 export default class ArticleService {
   @Inject()
   public Log(
-    @Param(['id', 'price'], new DefaultValuePipe('1000.99'), ParseIntPipe)
+    @Param(['id', 'price'], new DefaultValuePipe('1000.99'), new ParseIntPipe())
     record: number | Record<string, any>,
     @Param('name', new DefaultValuePipe('落魄前端'))
     name: string | Record<string, any>,
-    @Param('customElements', CustomValidationPipe)
+    @Param('customElements', new CustomValidationPipe())
     custom: string | Record<string, any>
   ): void {
     console.log('this is ArticleService', record, name, custom, 'custom')
