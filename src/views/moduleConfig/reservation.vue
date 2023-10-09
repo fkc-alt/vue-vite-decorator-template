3497cc5c8ac7b0ec050d1e9592f06ec6e90328a3
kaichao_feng <kaichao.feng@w-group.com.cn>
2023-10-09 09:37:19 +0800

    fix: interface body params

--- src/views/moduleConfig/reservation.vue
+++ src/views/moduleConfig/reservation.vue
@@ -60,8 +60,8 @@ const init = async () => {
     const [startTime, endTime] = time
     const [_startDate, _startTime] = startTime.split(' ')
     const [_endDate, _endTime] = endTime.split(' ')
-    DateReq.serviceStartDate = _startDate + ' 08:00:00'
-    DateReq.serviceEndDate = _endDate + ' 08:00:00'
+    DateReq.serviceStartDate = _startDate + ' 00:00:00'
+    DateReq.serviceEndDate = _endDate + ' 23:59:59'
     DateReq.serviceStartTime = _startTime
     DateReq.serviceEndTime = _endTime
   }
